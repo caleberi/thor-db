10be88c83db7060192328ff9e333ce08debb236e
caleberi <caleberioluwa@gmail.com>
2021-09-05 17:26:03 +0100

    added new cursor abstraction

--- src/utils/constant.c
+++ src/utils/constant.c
@@ -155,13 +155,21 @@ void deserialize_row(void *source, Row *destination)
 }
 
 
-void *row_slot(Table *table, uint32_t row_num)
-{
+
+void* cursor_value(Cursor* cursor){
+    uint32_t row_num = cursor->row_num;
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void* page = get_page(table->pager,page_num);
+    void* page = get_page(cursor->table->pager, page_num);
     uint32_t row_offset = row_num % ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * ROW_SIZE;
     return page + byte_offset;
+}
+
+void advance_cursor(Cursor* cursor){
+    cursor->row_num+=1;
+    if(cursor->row_num>=cursor->table->num_rows){
+        cursor->end_of_table=true;
+    }
 }
 
 MetaCommandResult do_meta_command(InputBuffer *input_buffer,Table* table)
@@ -203,19 +211,24 @@ ExecuteResult execute_insert(Statement *statement, Table *table)
         return EXECUTE_TABLE_FULL;
     }
     Row *row_to_insert = &(statement->row_to_insert);
+    Cursor* cursor = table_end(table);
     serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    serialize_row(row_to_insert, cursor_value(cursor));
     table->num_rows++;
+    free(cursor);
     return EXECUTE_SUCCESS;
 }
 
 ExecuteResult execute_select(Statement *statement, Table *table)
 {
+    Cursor* cursor = table_start(table);
     Row row;
-    for (uint32_t i = 0; i < table->num_rows; i++)
-    {
-        deserialize_row(row_slot(table, i), &row);
+    while (!(cursor->end_of_table)) {
+        deserialize_row(cursor_value(cursor), &row);
         print_row(&row);
+        cursor_advance(cursor);
     }
+    free(cursor);
     return EXECUTE_SUCCESS;
 }
 
@@ -228,4 +241,21 @@ ExecuteResult execute_statement(Statement *statement, Table *table)
     case STATEMENT_SELECT:
         return execute_select(statement, table);
     }
+}
+
+
+Cursor* table_start(Table* table){
+    Cursor* cursor =  malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+    return cursor;
+}
+
+Cursor* table_end(Table* table){
+    Cursor* cursor =  malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+    return cursor;
 }
